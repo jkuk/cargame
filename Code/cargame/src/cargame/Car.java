9715812521af345fb833da93ed1396ed1a62f1a4
David Tersoff <daverboy21@gmail.com>
2013-03-11 11:22:01 -0400

    removed junk, fixed drive method

--- Code/cargame/src/cargame/Car.java
+++ Code/cargame/src/cargame/Car.java
@@ -1,43 +1,23 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import java.util.*;
-=======
 import java.util.ArrayList;
->>>>>>> upstream/master
-=======
-package cargame;
+
 
 import java.util.ArrayList;
 import java.util.*;
->>>>>>> upstream/master
 import java.lang.Math;
 
 public class Car
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> upstream/master
   private double speed;
   private double mpg;
   private double tankSize;
   private double fuel;
   private double totalDistance;
   private double distance;
-<<<<<<< HEAD
-=======
-  private int speed;
-  private int mpg;
-  private int tankSize;
-  private int fuel;
-  private int distance;
->>>>>>> upstream/master
-=======
->>>>>>> upstream/master
-  private ArrayList<Stop> stopList;
   private Stop lastStop;
   private Stop nextStop;
-  private boolean raceFinished;
+  private ArrayList<Stop> stopList;
+    private boolean raceFinished;
   
   public Car(ArrayList<Stop> s)
   {
@@ -118,60 +98,56 @@ public class Car
   }
   public void setStop()
   {
-<<<<<<< HEAD
-<<<<<<< HEAD
     lastStop = nextStop;
     stopList.remove(lastStop);
-=======
+    
     stopList.remove(lastStop);
     lastStop = nextStop;
->>>>>>> upstream/master
-=======
+    
+    
     lastStop = nextStop;
     stopList.remove(lastStop);
->>>>>>> upstream/master
+    
     if(stopList.isEmpty())
       this.setRaceFinished(true);
     else
       nextStop = stopList.get(0);
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
+  
   public void findDistance()
   {
     totalDistance = Math.sqrt((lastStop.getX()-nextStop.getX())^2
-                        +(lastStop.getY()-nextStop.getY())^2);
-=======
+                                +(lastStop.getY()-nextStop.getY())^2);
+  }
   public int findDistance()
   {
     distance = Math.sqrt(Math.abs(lastStop.getX()-nextStop.getX())^2
-                        +Math.abs(lastStop.getY()-nextStop.getY())^2);
->>>>>>> upstream/master
-=======
-
+                           +Math.abs(lastStop.getY()-nextStop.getY())^2);
+    
+    
+  }
   public double findDistance()
   {
     totalDistance = Math.sqrt((lastStop.getX()-nextStop.getX())^2
-                        +(lastStop.getY()-nextStop.getY())^2);
+                                +(lastStop.getY()-nextStop.getY())^2);
     return totalDistance;
-
->>>>>>> upstream/master
+    
+    
   }
   public void refuel()
   {
     this.setFuel(tankSize);
   }
   public void drive() { //needs to occur on a step by step basis. or one step at a time.
-    for(int i = 1; i <= speed; i++)
+    
+    if(fuel == 0)
+      break;
+    else
     {
-      if(fuel == 0)
-        break;
-      else
-      {
-        distance -= speed;
-        fuel -= speed / mpg;
-      }
+      distance -= speed;
+      fuel -= speed / mpg;
     }
+    
   }
-      
+  
 }
